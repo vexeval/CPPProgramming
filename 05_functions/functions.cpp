e434d0e7ef7d8d016c2348830fe144eb2ecf415c
Matt <mshook7298@floridapoly.edu>
2025-08-28 20:19:09 +0000

    fumnctions

--- 05_functions/functions.cpp
+++ 05_functions/functions.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <cctype> // functions like to_upper, similar to ctype.h
+#include <cmath>
+#include <list>
 
 using namespace std;
 using std::cout;
@@ -9,12 +11,19 @@ using std::endl;
 // Function Headers
 void Print(string message);
 void Swap(int &a, int &b);
-void title(string &s);
+void Title(string &s);
+
+typedef struct VectorQ
+{
+    double x;
+    double y;
+} Vector2;
+Vector2 Quadratic(double a, double b, double c);
 
 int main(void)
 {
     string s1 = "hello";
-    title(s1);
+    Title(s1);
     cout << s1 << endl;
 
     Print("Yo\n");
@@ -23,6 +32,15 @@ int main(void)
     Print("Before Swap: a: " + to_string(a) + ", b: " + to_string(b) + "\n");
     Swap(a,b);
     Print("After Swap: a: " + to_string(a) + ", b: " + to_string(b) + "\n");
+
+
+    double x = -6;
+    double y = 4;
+    double z = sqrt(7);
+    Vector2 result = Quadratic(x, y, z);
+    cout << "Values a=" << x << ", b=" << y << ", c=" << z << endl;
+    Print("Result +x: " + to_string(result.x) + "\n");
+    Print("Result -x: " + to_string(result.y) + "\n");
 
     return 0;
 }
@@ -39,7 +57,13 @@ void Swap(int &a, int &b)
     b = t;
 }
 
-void title(string &s)
+void Title(string &s)
 {
     s[0] = toupper(s[0]);
+}
+
+Vector2 Quadratic(double a, double b, double c)
+{
+    Vector2 values = {(-b + sqrt(b*b - 4*a*c) / 2*a), (-b - sqrt(b*b - 4*a*c) / 2*a)};
+    return values;
 }
